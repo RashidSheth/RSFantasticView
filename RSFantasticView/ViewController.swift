eca910fb6c0a9b1e84b4e3f3cf25ab60c36348ba
Abdulrashid Shethwala <abdul.shethwala@rashid-ios.local>
2018-09-11 18:54:17 +0530

    Initial Commit

---
+++ RSFantasticView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSFantasticView
+//
+//  Created by Abdulrashid Shethwala on 9/11/18.
+//  Copyright © 2018 Abdulrashid Shethwala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
